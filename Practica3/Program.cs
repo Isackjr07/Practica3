b9d1cd46542f4dde21b870443e9e2cc9c47f6655
isack <isack@LAPTOP-KBT7Q9Q0>
2022-09-24 11:35:59 -0700

    Agregar archivos de proyecto.

---
+++ Practica3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lados;
+            double perimetro, medida;
+
+            Console.WriteLine("Cuantos lados tiene el poligono: ");
+            lados = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dame la medida de uno de sus lados: ");
+            medida = Convert.ToDouble(Console.ReadLine());
+
+            perimetro = lados * medida;
+
+            Console.WriteLine("El perimetro es :{0} ",perimetro);
+
+        }
+    }
+}
